2890f43311238ea213dfb95afc0fd9bf0578a20b
franck <carnet.franck.paul@gmail.com>
2015-06-30 18:01:41 +0200

    Cope with embed.ly mandatory API key

--- _admin.php
+++ _admin.php
@@ -14,14 +14,36 @@ if (!defined('DC_CONTEXT_ADMIN')) { return; }
 // dead but useful code, in order to have translations
 __('External Media').__('Insert external media from Internet');
 
+$core->addBehavior('adminBlogPreferencesForm',array('externalMediaBehaviors','adminBlogPreferencesForm'));
+$core->addBehavior('adminBeforeBlogSettingsUpdate',array('externalMediaBehaviors','adminBeforeBlogSettingsUpdate'));
 $core->addBehavior('adminPostEditor',array('externalMediaBehaviors','adminPostEditor'));
-
 $core->addBehavior('ckeditorExtraPlugins', array('externalMediaBehaviors', 'ckeditorExtraPlugins'));
 
 class externalMediaBehaviors
 {
+	public static function adminBlogPreferencesForm($core,$settings)
+	{
+		$settings->addNameSpace('extmedia');
+		echo
+		'<div class="fieldset"><h4>'.__('External Media').'</h4>'."\n".
+		'<p><label>'.
+		__('embed.ly API Key:')." ".
+		form::field('extmedia_api_key',25,50,$settings->extmedia->api_key,3).
+		'</label></p>'."\n".
+		'<p class="form-note">'.__('See <a href="https://app.embed.ly/signup">embed.ly</a> web site in order to get your free API Key.').'</p>'."\n".
+		'</div>'."\n";
+	}
+
+	public static function adminBeforeBlogSettingsUpdate($settings)
+	{
+		$settings->addNameSpace('extmedia');
+		$settings->extmedia->put('api_key',empty($_POST['extmedia_api_key'])? '' : $_POST['extmedia_api_key'],'string');
+	}
+
 	public static function adminPostEditor($editor='',$context='',array $tags=array())
 	{
+		global $core;
+
 		$res = '';
 		if ($editor == 'dcLegacyEditor') {
 
@@ -35,6 +57,7 @@ class externalMediaBehaviors
 
 		} elseif ($editor == 'dcCKEditor') {
 
+			$core->blog->settings->addNamespace('extmedia');
 			$res =
 				'<script type="text/javascript">'."\n"."//<![CDATA[\n".
 				dcPage::jsVar('extmedia_title',__('External media')).
@@ -47,8 +70,8 @@ class externalMediaBehaviors
 				dcPage::jsVar('extmedia_align_left',__('Left')).
 				dcPage::jsVar('extmedia_align_right',__('Right')).
 				dcPage::jsVar('extmedia_align_center',__('Center')).
+				dcPage::jsVar('extmedia_api_key',$core->blog->settings->extmedia->api_key).
 				"\n//]]>\n"."</script>\n";
-
 
 		}
 		return $res;
