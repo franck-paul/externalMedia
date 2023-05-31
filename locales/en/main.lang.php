c62dcc898248683d19ae3e674e9dafdbba8c0309
franck <carnet.franck.paul@gmail.com>
2023-05-31 10:07:58 +0200

    l10n

---
+++ locales/en/main.lang.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['External Media'] = '';
+L10n::$locales['Insert external media from Internet'] = '';
+L10n::$locales['Label:'] = '';
+L10n::$locales['See <a href="https://app.embed.ly/signup">embed.ly</a> web site in order to get your free API Key.'] = '';
+L10n::$locales['External media'] = '';
+L10n::$locales['URL'] = '';
+L10n::$locales['Page URL:'] = '';
+L10n::$locales['URL cannot be empty.'] = '';
+L10n::$locales['Alignment'] = '';
+L10n::$locales['Media alignment:'] = '';
+L10n::$locales['None'] = '';
+L10n::$locales['Left'] = '';
+L10n::$locales['Right'] = '';
+L10n::$locales['Center'] = '';
+L10n::$locales['embed.ly API Key missing, see blog settings'] = '';
+L10n::$locales['embed.ly API error: '] = '';
+L10n::$locales['External media selector'] = '';
+L10n::$locales['Supported media services'] = '';
+L10n::$locales['Please enter the URL of the page containing the video you want to include in your post.'] = '';
+L10n::$locales['Ok'] = '';
+L10n::$locales['Media alignment'] = '';
+L10n::$locales['Media title'] = '';
+L10n::$locales['Title:'] = '';
+L10n::$locales['Insert'] = '';
+L10n::$locales['Cancel'] = '';
