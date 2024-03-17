7a8bd2d4bb00fdb3d16ac609d5b9c24ad05131f4
franck <carnet.franck.paul@gmail.com>
2024-03-17 11:15:44 +0100

    New release 5.3

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'External Media',
     'Insert external media from Internet',
     'Olivier Meunier, Franck Paul and contributors',
-    '5.2',
+    '5.3',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
