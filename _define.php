7f053cc22f45a72b03a7096025c43819d1cb3ba7
franck <contact@open-time.net>
2026-04-08 19:58:44 +0200

    New release 6.4

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'External Media',
     'Insert external media from Internet',
     'Olivier Meunier, Franck Paul and contributors',
-    '6.3',
+    '6.4',
     [
-        'date'        => '2025-09-22T10:31:45+0200',
+        'date'        => '2026-04-08T19:58:44+0200',
         'requires'    => [['core', '2.36']],
         'permissions' => 'My',
         'type'        => 'plugin',
